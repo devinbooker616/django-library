39d56f2c83862df88a16892ddb29d06ed0cc925b
Base Camp <basecamp@ssa-406.local>
2019-11-18 13:55:01 -0600

    completed library

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Book(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    published = models.DateField(datetime.now())
+    genre = models.TextField()
+    in_stock = models.BooleanField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+
+class Transaction(models.Model):
+    datetime = models.DateField(datetime.now())
+    action = models.TextField()
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from app.models import Book, Transaction
+from datetime import datetime
 
-# Create your views here.
+def home(request):
+    books = Book.objects.all()
+    return render(request, "book_list.html", {"books": books} )
+
+def borrow_book(request, id):
+    book = Book.objects.get(id=id)
+    if book.in_stock:
+        messages.success(request, f"Borrowed {book.title} by {book.author}")
+        Checkout = book.transaction_set.create(datetime=datetime.now(), action="CHECKOUT", book=book)
+        book.in_stock = False
+        Checkout.save()
+        book.save()
+        return redirect("home")
+    else:
+        messages.error(request, f"{book.title} by {book.author} is unavailable")
+        return redirect("home")
+
+def return_book(request, id):           
+    book = Book.objects.get(id=id)
+    if book.in_stock == False:
+        messages.success(request, f"Returned {book.title} by {book.author}")
+        Checkin = book.transaction_set.create(datetime=datetime.now(), action="CHECKIN", book=book)
+        book.in_stock = True
+        Checkin.save()
+        book.save()
+        return redirect("home")
+    else:
+        messages.error(request, f"{book.title} by {book.author} is already here")
+        return redirect("home")
+
